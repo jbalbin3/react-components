6a1896e389a0bfeda27f72973e9d5d81208dd7ef
jackychenhahahaha <lifechen18@gmail.com>
2019-12-30 16:18:18 -0800

    react components completed

--- app.js
+++ app.js
@@ -1,1 +1,47 @@
 // TODO
+class GroceryListItem extends React.Component {
+
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      done: false
+    };
+  }
+
+  onListItemMouseEnter() {
+    this.setState({
+      done: !this.state.done
+    });
+  }
+
+  render() {
+    var style = {
+      fontWeight: this.state.done ? 'bold' : 'none'
+    };
+    return (
+      <li style={style} onMouseEnter={this.onListItemMouseEnter.bind(this)}>{this.props.grocery}</li>
+    );
+  }
+
+}
+
+var GroceryList = (props) => (
+  <ul>
+    {props.grocery.map(grocery =>
+      <GroceryListItem grocery={grocery} />
+    )}
+  </ul>
+);
+
+var App = () => (
+  <div>
+    <h2>My Grocery List</h2>
+    <GroceryList grocery={['Spinach', 'Turkey']}/>
+  </div>
+);
+
+ReactDOM.render(<App  />, document.getElementById("app"));
+
+
+
